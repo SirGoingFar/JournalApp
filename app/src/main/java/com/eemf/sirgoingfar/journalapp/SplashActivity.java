f2e940f451446f776badcf7657407a316f2ad743
Emmanuel_Akintunde <tunde@reach.africa>
2018-06-26 10:17:23 +0100

    Test Commit

--- app/src/main/java/com/eemf/sirgoingfar/journalapp/SplashActivity.java
+++ app/src/main/java/com/eemf/sirgoingfar/journalapp/SplashActivity.java
@@ -9,6 +9,7 @@ import butterknife.BindView;
 public class SplashActivity extends AppCompatActivity {
     @BindView(R.id.tv_name)
     TextView name;
+    TextView age;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
