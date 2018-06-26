ce31feb4b15d1f7c16896d5c55a0f39033c44f94
Emmanuel_Akintunde <tunde@reach.africa>
2018-06-26 10:09:29 +0100

    Test Commit

--- app/src/main/java/com/eemf/sirgoingfar/journalapp/SplashActivity.java
+++ app/src/main/java/com/eemf/sirgoingfar/journalapp/SplashActivity.java
@@ -2,8 +2,13 @@ package com.eemf.sirgoingfar.journalapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class SplashActivity extends AppCompatActivity {
+    @BindView(R.id.tv_name)
+    TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
